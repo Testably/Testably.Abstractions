4b974389b4a3f31419798b3a5936b080957eba00
Valentin Breuß <vbreuss@gmail.com>
2022-09-20 15:59:29 +0200

    feat: Ignore example tests if not enabled by a specific flag. (#7)
    Example tests are only executed and visible in the test explorer, if the
    following flag is set: `EXECUTE_EXAMPLE_TESTS`
    
    Co-authored-by: Valentin Breuß <v.breuss@tig.at>

---
+++ Docs/Examples/Examples.TimeSystem/ExampleTestAttribute.cs
@@ -0,0 +1,39 @@
+﻿#if EXECUTE_EXAMPLE_TESTS
+using Xunit;
+
+namespace Examples.TimeSystem;
+
+/// <summary>
+///     Activated example test attribute will execute the example unit tests. This was achieved by setting the
+///     "<c>EXECUTE_EXAMPLE_TESTS</c>" variable.
+///     <para />
+///     Remove this variable, if you no longer want to execute the example tests.
+/// </summary>
+public class ExampleTestAttribute
+    : FactAttribute
+{
+}
+#else
+using System;
+
+namespace Examples.TimeSystem;
+
+/// <summary>
+///     Used to illustrate example tests in this project.
+///     <para />
+///     These tests are not executed by default!
+///     <para />
+///     In order for these tests to execute, the variable "<c>EXECUTE_EXAMPLE_TESTS</c>" must be set,<br />
+///     e.g. in the .csproj file (see example for a snippet).
+/// </summary>
+/// <example>
+///     <PropertyGroup>
+///         <DefineConstants>$(DefineConstants);EXECUTE_EXAMPLE_TESTS</DefineConstants>
+///     </PropertyGroup>
+/// </example>
+public class ExampleTestAttribute
+    : Attribute
+{
+}
+
+#endif
