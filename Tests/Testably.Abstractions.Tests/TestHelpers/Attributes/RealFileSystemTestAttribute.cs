09db9ca4882386811233ffa6e4a3a62cc6903162
Valentin Breuß <vbreuss@gmail.com>
2022-09-28 21:47:59 +0200

    Use explicit property to disable tests on the real file system

--- Tests/Testably.Abstractions.Tests/TestHelpers/Attributes/RealFileSystemTestAttribute.cs
+++ Tests/Testably.Abstractions.Tests/TestHelpers/Attributes/RealFileSystemTestAttribute.cs
@@ -6,7 +6,7 @@
 public class RealFileSystemTestAttribute : RuntimeSwitchAttribute
 {
     public RealFileSystemTestAttribute()
-#if DEBUG
+#if DISABLE_TESTS_REALFILESYSTEM
         : base(false)
 #else
         : base(true)
