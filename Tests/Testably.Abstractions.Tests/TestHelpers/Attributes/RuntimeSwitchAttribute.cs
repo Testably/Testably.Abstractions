ca13e0bb75602328dfeb6d29c141173a7ddb3f20
Valentin Breuß <vbreuss@gmail.com>
2022-09-28 12:13:25 +0200

    feat: improve test discovery (#35)
    Create a custom test discovery mechanism so that test on the real file
    system are only executed on the build server or in release mode locally.

---
+++ Tests/Testably.Abstractions.Tests/TestHelpers/Attributes/RuntimeSwitchAttribute.cs
@@ -0,0 +1,24 @@
+﻿namespace Testably.Abstractions.Tests.TestHelpers.Attributes;
+
+/// <summary>
+///     Base attribute that implements <see cref="IRuntimeSwitch" />.
+/// </summary>
+[AttributeUsage(AttributeTargets.Class | AttributeTargets.Method)]
+public class RuntimeSwitchAttribute : Attribute, IRuntimeSwitch
+{
+    /// <summary>
+    ///     Initializes a new instance of <see cref="RuntimeSwitchAttribute" /> with <see cref="IsEnabled" /> set to
+    ///     <paramref name="isEnabled" />.
+    /// </summary>
+    public RuntimeSwitchAttribute(bool isEnabled)
+    {
+        IsEnabled = isEnabled;
+    }
+
+    #region IRuntimeSwitch Members
+
+    /// <inheritdoc cref="IRuntimeSwitch.IsEnabled" />
+    public bool IsEnabled { get; }
+
+    #endregion
+}
---
+++ Tests/Testably.Abstractions.Tests/TestHelpers/Framework/TestablyTestFramework.cs
@@ -0,0 +1,27 @@
+﻿using Testably.Abstractions.Tests.TestHelpers.Attributes;
+using Xunit.Abstractions;
+using Xunit.Sdk;
+[assembly: TestFramework(
+    "Testably.Abstractions.Tests.TestHelpers.Framework.TestablyTestFramework",
+    "Testably.Abstractions.Tests")]
+
+namespace Testably.Abstractions.Tests.TestHelpers.Framework;
+
+/// <summary>
+///     Custom <see cref="XunitTestFramework" /> that uses the <see cref="TestablyTestFrameworkDiscoverer" /> to support
+///     the <see cref="RuntimeSwitchAttribute" />.
+/// </summary>
+public class TestablyTestFramework : XunitTestFramework
+{
+    public TestablyTestFramework(IMessageSink messageSink)
+        : base(messageSink)
+    {
+    }
+
+    protected override ITestFrameworkDiscoverer CreateDiscoverer(
+        IAssemblyInfo assemblyInfo)
+        => new TestablyTestFrameworkDiscoverer(
+            assemblyInfo,
+            SourceInformationProvider,
+            DiagnosticMessageSink);
+}
