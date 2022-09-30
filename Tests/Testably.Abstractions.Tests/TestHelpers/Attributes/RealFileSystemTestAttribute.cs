644cc66852b49f9a8a4a7494e868dd5ea4b28f30
Valentin Breuß <vbreuss@gmail.com>
2022-09-30 08:26:42 +0200

    Windows with enabled RuntimeSwitchAttribute

--- Tests/Testably.Abstractions.Tests/TestHelpers/Attributes/RealFileSystemTestAttribute.cs
+++ Tests/Testably.Abstractions.Tests/TestHelpers/Attributes/RealFileSystemTestAttribute.cs
@@ -1,12 +1,17 @@
 ﻿namespace Testably.Abstractions.Tests.TestHelpers.Attributes;
 
 /// <summary>
-///     Disables execution of tests against the real file system in DEBUG mode.
+///     Disables execution of tests against the real file system in DEBUG mode
+///     when the `DISABLE_TESTS_REALFILESYSTEM` variable is set
 /// </summary>
-[AttributeUsage(AttributeTargets.Class | AttributeTargets.Method)]
-public class RealFileSystemTestAttribute : Attribute
+public class RealFileSystemTestAttribute : RuntimeSwitchAttribute
 {
     public RealFileSystemTestAttribute()
+#if DEBUG && DISABLE_TESTS_REALFILESYSTEM
+        : base(false)
+#else
+        : base(true)
+#endif
     {
     }
 }
