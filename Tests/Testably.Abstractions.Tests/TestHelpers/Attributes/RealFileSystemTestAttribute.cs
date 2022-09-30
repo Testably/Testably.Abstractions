4432d0553887272b3f7b29e9c2350b0027722afe
Valentin Breuß <vbreuss@gmail.com>
2022-09-30 08:47:02 +0200

    fix: Stryker not detecting all tests under ubuntu (#40)
    Run Stryker on Windows as it is only detecting a small subset of tests
    under ubuntu

--- Tests/Testably.Abstractions.Tests/TestHelpers/Attributes/RealFileSystemTestAttribute.cs
+++ Tests/Testably.Abstractions.Tests/TestHelpers/Attributes/RealFileSystemTestAttribute.cs
@@ -1,12 +1,13 @@
 ﻿namespace Testably.Abstractions.Tests.TestHelpers.Attributes;
 
 /// <summary>
-///     Disables execution of tests against the real file system in DEBUG mode.
+///     Disables execution of tests against the real file system in DEBUG mode
+///     when the `DISABLE_TESTS_REALFILESYSTEM` variable is set
 /// </summary>
 public class RealFileSystemTestAttribute : RuntimeSwitchAttribute
 {
     public RealFileSystemTestAttribute()
-#if DISABLE_TESTS_REALFILESYSTEM
+#if DEBUG && DISABLE_TESTS_REALFILESYSTEM
         : base(false)
 #else
         : base(true)
