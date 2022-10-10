2858232c0aa113e3bcfa0e8c263562cf418da043
Valentin Breuß <vbreuss@gmail.com>
2022-10-10 16:53:10 +0200

    Test edge case for File/Directory.Exists

---
+++ Tests/Testably.Abstractions.Tests/FileSystemDirectoryTests.Exists.cs
@@ -0,0 +1,14 @@
+namespace Testably.Abstractions.Tests;
+
+public abstract partial class FileSystemDirectoryTests<TFileSystem>
+    where TFileSystem : IFileSystem
+{
+    [SkippableFact]
+    [FileSystemTests.Directory(nameof(IFileSystem.IDirectory.Exists))]
+    public void Exists_Null_ShouldReturnFalse()
+    {
+        bool result = FileSystem.Directory.Exists(null);
+
+        result.Should().BeFalse();
+    }
+}
