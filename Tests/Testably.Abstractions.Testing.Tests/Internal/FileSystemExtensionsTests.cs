f30f79823d8b83d14d6a3c00ba149525aa961538
Valentin Breuß <vbreuss@gmail.com>
2022-10-10 21:04:55 +0200

    Add "Internal" unit tests

---
+++ Tests/Testably.Abstractions.Testing.Tests/Internal/FileSystemExtensionsTests.cs
@@ -0,0 +1,25 @@
+﻿using Testably.Abstractions.Testing.Internal;
+
+namespace Testably.Abstractions.Testing.Tests.Internal;
+
+public class FileSystemExtensionsTests
+{
+	[Theory]
+	[AutoData]
+	public void GetMoveLocation_LocationNotUnderSource_ShouldThrowNotSupportedException(
+		string location, string source, string destination)
+	{
+		FileSystemMock sut = new();
+		Exception? exception = Record.Exception(() =>
+		{
+			sut.GetMoveLocation(
+				sut.Storage.GetLocation(location),
+				sut.Storage.GetLocation(source),
+				sut.Storage.GetLocation(destination));
+		});
+
+		exception.Should().BeOfType<NotSupportedException>().Which.Message
+		   .Should().Contain($"'{sut.Path.GetFullPath(location)}'")
+		   .And.Contain($"'{sut.Path.GetFullPath(source)}'");
+	}
+}
---
+++ Tests/Testably.Abstractions.Testing.Tests/Internal/PathHelperTests.cs
@@ -0,0 +1,17 @@
+﻿using System.IO;
+using Testably.Abstractions.Testing.Internal;
+
+namespace Testably.Abstractions.Testing.Tests.Internal;
+
+public class PathHelperTests
+{
+	[Fact]
+	public void RemoveLeadingDot_MultipleLocalDirectories_ShouldBeRemoved()
+	{
+		string path = Path.Combine(".", ".", ".", "foo");
+
+		string result = path.RemoveLeadingDot();
+
+		result.Should().Be("foo");
+	}
+}
