641b87cc8fddb420df87816ae2cd40616c30f683
Valentin Breuß <vbreuss@gmail.com>
2022-10-12 16:56:13 +0200

    Add tests for NullContainer

---
+++ Tests/Testably.Abstractions.Testing.Tests/Storage/NullContainerTests.cs
@@ -0,0 +1,18 @@
+﻿using Testably.Abstractions.Testing.Storage;
+
+namespace Testably.Abstractions.Testing.Tests.Storage;
+
+public class NullContainerTests
+{
+	[Fact]
+	[Trait(nameof(Testing), nameof(NullContainer))]
+	public void Constructor_ShouldSetFileAndTimeSystem()
+	{
+		FileSystemMock fileSystem = new();
+
+		IStorageContainer sut = NullContainer.New(fileSystem);
+
+		sut.FileSystem.Should().Be(fileSystem);
+		sut.TimeSystem.Should().Be(fileSystem.TimeSystem);
+	}
+}
