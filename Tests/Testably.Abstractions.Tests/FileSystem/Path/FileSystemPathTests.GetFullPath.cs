60c8ec2c1f03486b67b887f77cd33d1f113eb939
Valentin Breuß <v.breuss@tig.at>
2022-10-17 08:21:46 +0200

    Check #425: Path normalization doesn't match standard C#'s functionality

---
+++ Tests/Testably.Abstractions.Tests/FileSystem/Path/FileSystemPathTests.GetFullPath.cs
@@ -0,0 +1,18 @@
+namespace Testably.Abstractions.Tests.FileSystem.Path;
+
+public abstract partial class FileSystemPathTests<TFileSystem>
+	where TFileSystem : IFileSystem
+{
+	[SkippableTheory]
+	[InlineData(@"top\..\most\file", @"most\file")]
+	[InlineData(@"top\..\most\..\dir\file", @"dir\file")]
+	[InlineData(@"top\../..\most\file", @"most\file")]
+	public void GetFullPath_ShouldNormalizeProvidedPath(string input, string expected)
+	{
+		string expectedRootedPath = FileTestHelper.RootDrive(expected);
+
+		string result = FileSystem.Path.GetFullPath(FileTestHelper.RootDrive(input));
+
+		result.Should().Be(expectedRootedPath);
+	}
+}
