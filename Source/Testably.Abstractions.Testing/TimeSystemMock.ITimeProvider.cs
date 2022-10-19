062966fe05ed7885e81c0cb31f832878a2f1bdb2
Valentin Breuß <vbreuss@gmail.com>
2022-10-19 07:55:43 +0200

    Remove Thread-Aware time provider

--- Source/Testably.Abstractions.Testing/TimeSystemMock.ITimeProvider.cs
+++ Source/Testably.Abstractions.Testing/TimeSystemMock.ITimeProvider.cs
@@ -39,15 +39,5 @@ public sealed partial class TimeSystemMock
 		///     Sets the currently simulated system time to the specified <paramref name="value" />.
 		/// </summary>
 		void SetTo(DateTime value);
-
-		/// <summary>
-		///     Synchronizes the currently simulated system time across all async contexts.
-		/// </summary>
-		/// <remarks>
-		///     This means that in multi-thread or async environments after this call all clocks start with the value from the
-		///     calling async context.<br />
-		///     (see also <see href="https://learn.microsoft.com/en-us/dotnet/api/system.threading.asynclocal-1" />)
-		/// </remarks>
-		void SynchronizeClock();
 	}
 }
