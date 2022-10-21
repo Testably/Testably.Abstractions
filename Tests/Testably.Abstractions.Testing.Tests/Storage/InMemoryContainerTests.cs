6a4ac4c3185182f6f1cf9d16a8c396a54de88126
Valentin Breuß <vbreuss@gmail.com>
2022-10-21 12:06:09 +0200

    InMemoryContainer tests

--- Tests/Testably.Abstractions.Testing.Tests/Storage/InMemoryContainerTests.cs
+++ Tests/Testably.Abstractions.Testing.Tests/Storage/InMemoryContainerTests.cs
@@ -1,10 +1,102 @@
 ﻿using System.IO;
 using Testably.Abstractions.Testing.Storage;
+using Testably.Abstractions.Testing.Tests.TestHelpers;
 
 namespace Testably.Abstractions.Testing.Tests.Storage;
 
 public class InMemoryContainerTests
 {
+	[Theory]
+	[AutoData]
+	public void AdjustAttributes_Decrypt_ShouldNotHaveEncryptedAttribute(string path)
+	{
+		Testing.FileSystemMock fileSystem = new();
+		Testing.FileSystemMock.DriveInfoMock drive =
+			Testing.FileSystemMock.DriveInfoMock.New("C", fileSystem);
+		IStorageLocation location = InMemoryLocation.New(drive,
+			fileSystem.Path.GetFullPath(path));
+
+		InMemoryContainer container = new(FileSystemTypes.File, location, fileSystem);
+		container.Encrypt();
+		container.Decrypt();
+
+		FileAttributes result = container.AdjustAttributes(FileAttributes.Normal);
+
+		result.Should().NotHaveFlag(FileAttributes.Encrypted);
+	}
+
+	[Theory]
+	[AutoData]
+	public void AdjustAttributes_Encrypt_ShouldHaveEncryptedAttribute(string path)
+	{
+		Testing.FileSystemMock fileSystem = new();
+		Testing.FileSystemMock.DriveInfoMock drive =
+			Testing.FileSystemMock.DriveInfoMock.New("C", fileSystem);
+		IStorageLocation location = InMemoryLocation.New(drive,
+			fileSystem.Path.GetFullPath(path));
+
+		InMemoryContainer container = new(FileSystemTypes.File, location, fileSystem);
+		container.Encrypt();
+
+		FileAttributes result = container.AdjustAttributes(FileAttributes.Normal);
+
+		result.Should().HaveFlag(FileAttributes.Encrypted);
+	}
+
+	[Theory]
+	[AutoData]
+	public void AdjustAttributes_LeadingDot_ShouldBeHiddenOnLinux(string path)
+	{
+		path = "." + path;
+		Testing.FileSystemMock fileSystem = new();
+		Testing.FileSystemMock.DriveInfoMock drive =
+			Testing.FileSystemMock.DriveInfoMock.New("C", fileSystem);
+		IStorageLocation location = InMemoryLocation.New(drive,
+			fileSystem.Path.GetFullPath(path));
+
+		InMemoryContainer container = new(FileSystemTypes.File, location, fileSystem);
+
+		FileAttributes result = container.AdjustAttributes(FileAttributes.Normal);
+
+		if (Test.RunsOnLinux)
+		{
+			result.Should().HaveFlag(FileAttributes.Hidden);
+		}
+		else
+		{
+			result.Should().NotHaveFlag(FileAttributes.Hidden);
+		}
+	}
+
+#if FEATURE_FILESYSTEM_LINK
+	[Theory]
+	[InlineAutoData(null, false)]
+	[InlineAutoData("foo", true)]
+	public void AdjustAttributes_ShouldHaveReparsePointAttributeWhenLinkTargetIsNotNull(
+		string? linkTarget, bool shouldHaveReparsePoint, string path)
+	{
+		Testing.FileSystemMock fileSystem = new();
+		Testing.FileSystemMock.DriveInfoMock drive =
+			Testing.FileSystemMock.DriveInfoMock.New("C", fileSystem);
+		IStorageLocation location = InMemoryLocation.New(drive,
+			fileSystem.Path.GetFullPath(path));
+
+		InMemoryContainer container = new(FileSystemTypes.File, location, fileSystem);
+		container.LinkTarget = linkTarget;
+
+		FileAttributes result = container.AdjustAttributes(FileAttributes.Normal);
+
+		if (shouldHaveReparsePoint)
+		{
+			result.Should().HaveFlag(FileAttributes.ReparsePoint);
+		}
+		else
+		{
+			result.Should().NotHaveFlag(FileAttributes.ReparsePoint);
+		}
+	}
+#endif
+
 	[Theory]
 	[AutoData]
 	public void Decrypt_Encrypted_ShouldDecryptBytes(
