69bae159656138e45b35939a5e4ee933f7e6d0f3
Valentin Breuß <vbreuss@gmail.com>
2022-10-22 17:41:08 +0200

    Add tests for ExtensionContainer

---
+++ Tests/Testably.Abstractions.Tests/FileSystem/FileSystemTests.ExtensionContainer.cs
@@ -0,0 +1,92 @@
+namespace Testably.Abstractions.Tests.FileSystem;
+
+public abstract partial class FileSystemTests<TFileSystem>
+	where TFileSystem : IFileSystem
+{
+	[SkippableTheory]
+	[AutoData]
+	public void
+		ExtensionContainer_HasWrappedInstance_WithCorrectType_ShouldReturnTrueOnRealFileSystem(
+			string name)
+	{
+		bool result = FileSystem.FileInfo.New(name).ExtensionContainer
+		   .HasWrappedInstance(out System.IO.FileInfo? fileInfo);
+
+		if (FileSystem is Abstractions.FileSystem)
+		{
+			result.Should().BeTrue();
+			fileInfo!.Name.Should().Be(name);
+		}
+		else
+		{
+			result.Should().BeFalse();
+		}
+	}
+
+	[SkippableTheory]
+	[AutoData]
+	public void
+		ExtensionContainer_HasWrappedInstance_WithIncorrectType_ShouldReturnAlwaysFalse(
+			string name)
+	{
+		bool result = FileSystem.FileInfo.New(name).ExtensionContainer
+		   .HasWrappedInstance(out System.IO.DirectoryInfo? directoryInfo);
+
+		result.Should().BeFalse();
+		directoryInfo.Should().BeNull();
+	}
+
+	[SkippableTheory]
+	[AutoData]
+	public void
+		ExtensionContainer_RetrieveMetadata_CorrectKeyAndType_ShouldReturnStoredValue(
+			string name, DateTime time)
+	{
+		IFileSystem.IFileSystemExtensionContainer sut = FileSystem.FileInfo.New(name)
+		   .ExtensionContainer;
+
+		sut.StoreMetadata("foo", time);
+		DateTime? result = sut.RetrieveMetadata<DateTime?>("foo");
+
+		result.Should().Be(time);
+	}
+
+	[SkippableTheory]
+	[AutoData]
+	public void ExtensionContainer_RetrieveMetadata_DifferentKey_ShouldReturnNull(
+		string name)
+	{
+		IFileSystem.IFileSystemExtensionContainer sut = FileSystem.FileInfo.New(name)
+		   .ExtensionContainer;
+
+		sut.StoreMetadata("foo", DateTime.Now);
+		DateTime? result = sut.RetrieveMetadata<DateTime?>("bar");
+
+		result.Should().BeNull();
+	}
+
+	[SkippableTheory]
+	[AutoData]
+	public void ExtensionContainer_RetrieveMetadata_DifferentType_ShouldReturnNull(
+		string name)
+	{
+		IFileSystem.IFileSystemExtensionContainer sut = FileSystem.FileInfo.New(name)
+		   .ExtensionContainer;
+
+		sut.StoreMetadata("foo", DateTime.Now);
+		TimeSpan? result = sut.RetrieveMetadata<TimeSpan?>("foo");
+
+		result.Should().BeNull();
+	}
+
+	[SkippableTheory]
+	[AutoData]
+	public void ExtensionContainer_RetrieveMetadata_NotRegisteredKey_ShouldReturnNull(
+		string name)
+	{
+		object? result = FileSystem.FileInfo.New(name).ExtensionContainer
+		   .RetrieveMetadata<object?>("foo");
+
+		result.Should().BeNull();
+	}
+}
