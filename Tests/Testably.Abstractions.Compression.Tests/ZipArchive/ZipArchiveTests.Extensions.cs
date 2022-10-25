f88495c897df5f7b4e8ce6297bf13c90069a273a
Valentin Breuß <vbreuss@gmail.com>
2022-10-25 15:52:54 +0200

    Add first test

---
+++ Tests/Testably.Abstractions.Compression.Tests/ZipArchive/ZipArchiveTests.Extensions.cs
@@ -0,0 +1,21 @@
+﻿using System.IO.Compression;
+
+namespace Testably.Abstractions.Compression.Tests.ZipArchive;
+
+public abstract partial class ZipArchiveTests<TFileSystem>
+	where TFileSystem : IFileSystem
+{
+	[SkippableTheory]
+	[AutoData]
+	public void ExtractToDirectory_Null_ShouldThrowException(
+		CompressionLevel compressionLevel)
+	{
+		IZipArchive? archive = null;
+		Exception? exception = Record.Exception(() =>
+		{
+			archive!.ExtractToDirectory("foo");
+		});
+
+		exception.Should().BeOfType<ArgumentNullException>();
+	}
+}
