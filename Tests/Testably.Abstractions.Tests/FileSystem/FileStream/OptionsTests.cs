1bf9cc3f84043c43ceee8679d8bfa9ea6daa8a77
Valentin Breuß <vbreuss@gmail.com>
2022-11-05 08:23:24 +0100

    Add tests for encryption

--- Tests/Testably.Abstractions.Tests/FileSystem/FileStream/OptionsTests.cs
+++ Tests/Testably.Abstractions.Tests/FileSystem/FileStream/OptionsTests.cs
@@ -1,4 +1,5 @@
 using System.IO;
+using System.Text;
 using Testably.Abstractions.FileSystem;
 
 namespace Testably.Abstractions.Tests.FileSystem.FileStream;
@@ -36,5 +37,26 @@ public abstract partial class OptionsTests<TFileSystem>
 		stream.Dispose();
 
 		FileSystem.File.Exists(path).Should().BeFalse();
+	}
+
+	[SkippableTheory]
+	[AutoData]
+	public void Options_Encrypt_ShouldDeleteFileOnDispose(
+		string path, string contents1, string contents2)
+	{
+		FileSystem.File.WriteAllText(path, contents1);
+		FileSystem.File.Encrypt(path);
+
+		using FileSystemStream stream = FileSystem.FileStream.New(path, FileMode.Open,
+			FileAccess.ReadWrite, FileShare.None, 10, FileOptions.Encrypted);
+		var bytes = Encoding.Default.GetBytes(contents2);
+
+		stream.Write(bytes, 0, bytes.Length);
+		stream.Dispose();
+
+		FileSystem.File.GetAttributes(path).Should().HaveFlag(FileAttributes.Encrypted);
+		FileSystem.File.ReadAllText(path).Should().NotBe(contents2);
+		FileSystem.File.Decrypt(path);
+		FileSystem.File.ReadAllText(path).Should().Be(contents2);
 	}
 }
