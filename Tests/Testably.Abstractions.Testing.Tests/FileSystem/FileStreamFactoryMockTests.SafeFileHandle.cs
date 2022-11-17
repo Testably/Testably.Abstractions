3c83605692fcddde6c732377cfa94a3d493f36ba
Valentin Breuß <vbreuss@gmail.com>
2022-11-17 14:50:42 +0100

    Add tests

--- Tests/Testably.Abstractions.Testing.Tests/FileSystem/FileStreamFactoryMockTests.SafeFileHandle.cs
+++ Tests/Testably.Abstractions.Testing.Tests/FileSystem/FileStreamFactoryMockTests.SafeFileHandle.cs
@@ -1,12 +1,36 @@
 ﻿#if EXECUTE_SAFEFILEHANDLE_TESTS
 using Microsoft.Win32.SafeHandles;
 using System.IO;
+using Testably.Abstractions.Testing.FileSystem;
 using Testably.Abstractions.Testing.Tests.TestHelpers;
 
 namespace Testably.Abstractions.Testing.Tests.FileSystem;
 
 public sealed partial class FileStreamFactoryMockTests
 {
+	[SkippableTheory]
+	[AutoData]
+	public void MissingFile_ShouldThrowFileNotFoundException(
+		string path, string contents)
+	{
+		path = RealFileSystem.Path.GetFullPath(path);
+		RealFileSystem.File.WriteAllText(path, contents);
+
+		using SafeFileHandle handle = UnmanagedFileLoader.CreateSafeFileHandle(path);
+		handle.IsInvalid.Should().BeFalse();
+		MockFileSystem.WithSafeFileHandleStrategy(
+			new DefaultSafeFileHandleStrategy(_ => new SafeFileHandleMock(path)));
+
+		Exception? exception = Record.Exception(() =>
+		{
+			// ReSharper disable once AccessToDisposedClosure
+			MockFileSystem.FileStream.New(handle, FileAccess.Read);
+		});
+
+		exception.Should().BeOfType<FileNotFoundException>()
+			.Which.Message.Should().Contain($"'{MockFileSystem.Path.GetFullPath(path)}'");
+	}
+
 	[SkippableTheory]
 	[AutoData]
 	public void UnregisteredFileHandle_ShouldThrowNotSupportedException(
