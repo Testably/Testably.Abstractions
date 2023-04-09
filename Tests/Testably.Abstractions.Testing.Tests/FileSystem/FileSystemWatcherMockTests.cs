6a35084e1e6a3c9dbfe271c22272f021d993d0ee
Valentin Breuß <v.breuss@tig.at>
2023-04-09 21:46:31 +0200

    Skip brittle test

--- Tests/Testably.Abstractions.Testing.Tests/FileSystem/FileSystemWatcherMockTests.cs
+++ Tests/Testably.Abstractions.Testing.Tests/FileSystem/FileSystemWatcherMockTests.cs
@@ -140,6 +140,8 @@ public sealed class FileSystemWatcherMockTests : IDisposable
 	[AutoData]
 	public void InternalBufferSize_ShouldResetQueue(string path1, string path2)
 	{
+		Skip.If(true, "Brittle test fails on build system (disabled in #284)");
+
 		using IFileSystemWatcher fileSystemWatcher =
 			FileSystem.FileSystemWatcher.New(BasePath);
 		ManualResetEventSlim block1 = new();
