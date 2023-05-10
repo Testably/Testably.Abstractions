0f42e159affe4e85b40a23772f9092112009bfb6
Valentin Breuß <v.breuss@tig.at>
2023-05-10 18:29:46 +0200

    Intermediate commit 2

---
+++ Examples/FileSystemWatcher/FileSystemWatcher.Tests/FileSystemWatcherTests.cs
@@ -0,0 +1,35 @@
+﻿using FluentAssertions;
+using System.IO;
+using System.IO.Abstractions;
+using System.Threading;
+using Testably.Abstractions.Testing;
+using Xunit;
+
+namespace Testably.Abstractions.Examples.FileSystemWatcher.Tests;
+
+public class FileSystemWatcherTests
+{
+	[Fact]
+	public void EnableRaisingEvents_ShouldTriggerWhenFileSystemChanges()
+	{
+		var fileSystem = new MockFileSystem();
+		fileSystem.Initialize();
+		FileSystemEventArgs? result = null;
+		ManualResetEventSlim ms = new();
+		using IFileSystemWatcher fileSystemWatcher =
+			fileSystem.FileSystemWatcher.New(".");
+		fileSystemWatcher.Created += (_, eventArgs) =>
+		{
+			result = eventArgs;
+			ms.Set();
+		};
+		fileSystemWatcher.NotifyFilter = NotifyFilters.DirectoryName;
+		fileSystemWatcher.EnableRaisingEvents = true;
+
+		fileSystem.Directory.CreateDirectory("foo");
+
+		ms.Wait(1000).Should().BeTrue();
+
+		result.Should().NotBeNull();
+	}
+}
