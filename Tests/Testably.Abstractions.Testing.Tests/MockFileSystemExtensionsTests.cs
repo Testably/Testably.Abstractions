8e76e0e03acae5d8e20981b4a4992ec50212c586
Valentin Breuß <vbreuss@gmail.com>
2023-08-21 21:08:52 +0200

    Add ToString() tests

---
+++ Tests/Testably.Abstractions.Testing.Tests/MockFileSystemExtensionsTests.cs
@@ -0,0 +1,21 @@
+﻿using Testably.Abstractions.Testing.Helpers;
+using Testably.Abstractions.Testing.Storage;
+
+namespace Testably.Abstractions.Testing.Tests;
+
+public class MockFileSystemExtensionsTests
+{
+	[Fact]
+	public void GetDefaultDrive_WithoutDrives_ShouldThrowInvalidOperationException()
+	{
+		MockFileSystem fileSystem = new();
+		(fileSystem.Storage as InMemoryStorage)?.RemoveDrive(string.Empty.PrefixRoot());
+
+		Exception? exception = Record.Exception(() =>
+		{
+			fileSystem.GetDefaultDrive();
+		});
+
+		exception.Should().BeOfType<InvalidOperationException>();
+	}
+}
