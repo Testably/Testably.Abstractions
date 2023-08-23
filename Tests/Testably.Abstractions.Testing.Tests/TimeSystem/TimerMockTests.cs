6f8482c2f9f82643aaca990f22a061ba9ae6c631
Valentin Breuß <vbreuss@gmail.com>
2023-08-23 17:37:35 +0200

    Fix failing tests in TimerMock

--- Tests/Testably.Abstractions.Testing.Tests/TimeSystem/TimerMockTests.cs
+++ Tests/Testably.Abstractions.Testing.Tests/TimeSystem/TimerMockTests.cs
@@ -69,10 +69,14 @@ public class TimerMockTests
 			timer.Change(0, 0);
 		});
 
+#if NET8_0_OR_GREATER
+		exception.Should().BeNull();
+#else
 		exception.Should().BeOfType<ObjectDisposedException>()
 			.Which.Message.Should().ContainAll(
 				"Cannot access a disposed object.",
 				nameof(ITimer.Change));
+#endif
 	}
 
 #if FEATURE_ASYNC_DISPOSABLE
@@ -90,7 +94,11 @@ public class TimerMockTests
 			timer.Change(0, 0);
 		});
 
+#if NET8_0_OR_GREATER
+		exception.Should().BeNull();
+#else
 		exception.Should().BeOfType<ObjectDisposedException>();
+#endif
 	}
 #endif
 
