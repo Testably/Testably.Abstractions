8531ba18f3733781d736db0ee45b383ccf5f0e34
Valentin Breuß <vbreuss@gmail.com>
2023-08-23 10:43:28 +0200

    Add tests for ExceptionFactory

---
+++ Tests/Testably.Abstractions.Testing.Tests/Helpers/ExceptionFactoryTests.cs
@@ -0,0 +1,30 @@
+﻿using Testably.Abstractions.Testing.Helpers;
+
+namespace Testably.Abstractions.Testing.Tests.Helpers;
+
+public sealed class ExceptionFactoryTests
+{
+	[Fact]
+	public void NotSupportedSafeFileHandle_ShouldMentionWithSafeFileHandleStrategy()
+	{
+		NotSupportedException sut = ExceptionFactory.NotSupportedSafeFileHandle();
+
+		sut.Message.Should().Contain(nameof(MockFileSystem.WithSafeFileHandleStrategy));
+	}
+
+	[Fact]
+	public void OperationNotSupportedOnThisPlatform_ShouldMentionPlatform()
+	{
+		PlatformNotSupportedException sut = ExceptionFactory.OperationNotSupportedOnThisPlatform();
+
+		sut.Message.Should().Contain("platform");
+	}
+
+	[Fact]
+	public void SearchPatternCannotContainTwoDots_ShouldMentionTwoDots()
+	{
+		ArgumentException sut = ExceptionFactory.SearchPatternCannotContainTwoDots();
+
+		sut.Message.Should().Contain("\"..\"");
+	}
+}
