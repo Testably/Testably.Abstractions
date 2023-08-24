4fed908130fa7f987146f738011c7de0be4b9162
Valentin Breuß <vbreuss@gmail.com>
2023-08-24 19:56:33 +0200

    Enable .NET 8.0

---
+++ Tests/Testably.Abstractions.Parity.Tests/Net8ParityTests.cs
@@ -0,0 +1,18 @@
+﻿#if NET8_0
+using System.IO;
+using Xunit.Abstractions;
+
+namespace Testably.Abstractions.Parity.Tests;
+
+// ReSharper disable once UnusedMember.Global
+public class Net8ParityTests : ParityTests
+{
+	public Net8ParityTests(ITestOutputHelper testOutputHelper)
+		: base(new TestHelpers.Parity(), testOutputHelper)
+	{
+		Parity.File.MissingMethods.Add(
+			typeof(File).GetMethod(nameof(File.OpenHandle)));
+	}
+}
+
+#endif
