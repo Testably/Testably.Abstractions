57ff1e337ae3a81df57124b8fb4ea047195fca59
Valentin Breuß <vbreuss@gmail.com>
2023-08-24 16:54:54 +0200

    Adjust tests for MacOS

--- Tests/Testably.Abstractions.Tests/FileSystem/File/CopyTests.cs
+++ Tests/Testably.Abstractions.Tests/FileSystem/File/CopyTests.cs
@@ -156,11 +156,18 @@ public abstract partial class CopyTests<TFileSystem>
 		sourceCreationTime.Should()
 			.BeOnOrAfter(creationTimeStart.ApplySystemClockTolerance()).And
 			.BeOnOrBefore(creationTimeEnd);
+		if (Test.RunsOnMac)
+		{
 #if NET8_0_OR_GREATER
-		if (Test.RunsOnWindows)
+			sourceLastAccessTime.Should()
+				.BeOnOrAfter(creationTimeStart.ApplySystemClockTolerance()).And
+				.BeOnOrBefore(creationTimeEnd);
 #else
-		if (!Test.RunsOnLinux)
+			sourceLastAccessTime.Should()
+				.BeOnOrAfter(updateTime.ApplySystemClockTolerance());
 #endif
+		}
+		else if (Test.RunsOnWindows)
 		{
 			sourceLastAccessTime.Should()
 				.BeOnOrAfter(creationTimeStart.ApplySystemClockTolerance()).And
