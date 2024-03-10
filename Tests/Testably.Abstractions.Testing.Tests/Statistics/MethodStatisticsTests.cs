16389e4b34352a6bd7847034ae7ba466e92f4927
Valentin Breuß <vbreuss@gmail.com>
2024-03-10 12:28:23 +0100

    Add missing tests

---
+++ Tests/Testably.Abstractions.Testing.Tests/Statistics/MethodStatisticsTests.cs
@@ -0,0 +1,31 @@
+﻿namespace Testably.Abstractions.Testing.Tests.Statistics;
+
+public sealed class MethodStatisticsTests
+{
+	[Fact]
+	public void ToString_ShouldContainName()
+	{
+		MockFileSystem sut = new();
+		sut.Directory.CreateDirectory("foo");
+
+		string result = sut.Statistics.Directory.Methods[1].ToString();
+
+		result.Should()
+			.Contain(nameof(IDirectory.CreateDirectory)).And
+			.Contain("\"foo\"").And
+			.NotContain(",");
+	}
+
+	[Fact]
+	public void ToString_ShouldContainParameters()
+	{
+		MockFileSystem sut = new();
+		sut.File.WriteAllText("foo", "bar");
+
+		string result = sut.Statistics.File.Methods[1].ToString();
+
+		result.Should()
+			.Contain(nameof(IFile.WriteAllText)).And
+			.Contain("\"foo\",\"bar\"");
+	}
+}
