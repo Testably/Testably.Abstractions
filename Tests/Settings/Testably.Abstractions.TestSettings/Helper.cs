852c5adde1df2ebcc7797f7aaa5be39a3f697317
Valentin Breuß <vbreuss@gmail.com>
2024-03-17 17:26:49 +0100

    Fix tests

---
+++ Tests/Settings/Testably.Abstractions.TestSettings/Helper.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.IO;
+using System.Text.Json;
+
+namespace Testably.Abstractions.TestSettings;
+
+internal static class Helper
+{
+	public static TestHelpers.Settings.TestSettings ChangeTestSettings(
+		Action<TestHelpers.Settings.TestSettings> change)
+	{
+		TestHelpers.Settings.TestSettings settings = ReadTestSettings();
+		change(settings);
+		WriteTestSettings(settings);
+		return settings;
+	}
+
+	private static string GetTestSettingsPath() =>
+		Path.GetFullPath(Path.Combine("..", "..", "..", "..", "test.settings.json"));
+
+	private static TestHelpers.Settings.TestSettings ReadTestSettings()
+	{
+		try
+		{
+			string path = GetTestSettingsPath();
+			string content = File.ReadAllText(path);
+			return JsonSerializer.Deserialize<TestHelpers.Settings.TestSettings>(content)
+			       ?? throw new NotSupportedException("The file has an invalid syntax!");
+		}
+		catch (Exception)
+		{
+			return new TestHelpers.Settings.TestSettings();
+		}
+	}
+
+	private static void WriteTestSettings(TestHelpers.Settings.TestSettings settings)
+	{
+		string content = JsonSerializer.Serialize(settings, new JsonSerializerOptions
+		{
+			WriteIndented = true
+		});
+		string path = GetTestSettingsPath();
+		File.WriteAllText(path, content);
+	}
+}
---
+++ Tests/Settings/Testably.Abstractions.TestSettings/RealFileSystemTestsInDebugMode.cs
@@ -0,0 +1,38 @@
+﻿using NUnit.Framework;
+
+namespace Testably.Abstractions.TestSettings;
+
+public sealed class RealFileSystemTestsInDebugMode
+{
+	/// <summary>
+	///     If disabled, the classes for executing tests against the real file system no longer run in DEBUG mode.
+	/// </summary>
+	[TestCase]
+	[Explicit]
+	public void Disable()
+	{
+		TestHelpers.Settings.TestSettings result = Helper.ChangeTestSettings(s =>
+			s.EnableRealFileSystemTestsInDebugMode = false);
+
+		Assert.Pass("Tests against the real file system are disabled in DEBUG mode.");
+	}
+
+	/// <summary>
+	///     If enabled, the classes for executing tests against the real file system also run in DEBUG mode.
+	/// </summary>
+	[TestCase]
+	[Explicit]
+	public void Enable()
+	{
+		TestHelpers.Settings.TestSettings result = Helper.ChangeTestSettings(s =>
+			s.EnableRealFileSystemTestsInDebugMode = true);
+
+		Assert.Pass("Tests against the real file system are enabled in DEBUG mode.");
+	}
+
+	[TestCase]
+	public void TestDummy_SoThatExplicitTestsAreIgnored()
+	{
+		Assert.Pass();
+	}
+}
