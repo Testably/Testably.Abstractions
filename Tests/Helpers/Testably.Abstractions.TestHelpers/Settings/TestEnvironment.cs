02b900c959b4560921069e9a9aa6807b154d650c
Valentin <valentin.breuss@baur.at>
2024-03-19 16:35:05 +0100

    Cleanup documentation

--- Tests/Helpers/Testably.Abstractions.TestHelpers/Settings/TestEnvironment.cs
+++ Tests/Helpers/Testably.Abstractions.TestHelpers/Settings/TestEnvironment.cs
@@ -3,18 +3,18 @@
 public class TestEnvironment
 {
 	/// <summary>
+	///     Affects some tests, that are brittle on the real file system.
+	/// </summary>
+	/// <remarks>Per default, they are <see cref="TestSettingStatus.AlwaysDisabled" />.</remarks>
+	public TestSettingStatus BrittleTests { get; set; }
+		= TestSettingStatus.AlwaysDisabled;
+
+	/// <summary>
 	///     Affects some tests, that take a long time to run against the real file system (e.g. timeout).
 	/// </summary>
 	/// <remarks>Per default, they are <see cref="TestSettingStatus.DisabledInDebugMode" />.</remarks>
 	public TestSettingStatus LongRunningTests { get; set; }
 		= TestSettingStatus.DisabledInDebugMode;
-
-	/// <summary>
-	///     Affects some tests, that are brittle on the real file system.
-	/// </summary>
-	/// <remarks>Per default, they are <see cref="TestSettingStatus.DisabledInDebugMode" />.</remarks>
-	public TestSettingStatus BrittleTests { get; set; }
-		= TestSettingStatus.AlwaysDisabled;
 
 	/// <summary>
 	///     Affects all tests against the real file system.
