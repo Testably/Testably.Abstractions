1b79aaf837339e91ae8a2a895878c0d3ff24cb1e
Valentin Breuß <vbreuss@gmail.com>
2024-04-07 18:44:11 +0200

    Add missing Execute tests

--- Tests/Testably.Abstractions.Compression.Tests/Internal/ExecuteTests.cs
+++ Tests/Testably.Abstractions.Compression.Tests/Internal/ExecuteTests.cs
@@ -5,28 +5,80 @@ namespace Testably.Abstractions.Compression.Tests.Internal;
 public sealed class ExecuteTests
 {
 	[Fact]
-	public void WhenRealFileSystem_MockFileSystem_ShouldExecuteOnMockFileSystem()
+	public void WhenRealFileSystem_MockFileSystem_WithActionCallback_ShouldExecuteOnMockFileSystem()
 	{
 		bool onRealFileSystemExecuted = false;
 		bool onMockFileSystemExecuted = false;
 		MockFileSystem fileSystem = new();
 		Execute.WhenRealFileSystem(fileSystem,
-			() => onRealFileSystemExecuted = true,
-			() => onMockFileSystemExecuted = true);
+			() =>
+			{
+				onRealFileSystemExecuted = true;
+			},
+			() =>
+			{
+				onMockFileSystemExecuted = true;
+			});
 
 		onRealFileSystemExecuted.Should().BeFalse();
 		onMockFileSystemExecuted.Should().BeTrue();
 	}
 
 	[Fact]
-	public void WhenRealFileSystem_RealFileSystem_ShouldExecuteOnRealFileSystem()
+	public void WhenRealFileSystem_MockFileSystem_WithFuncCallback_ShouldExecuteOnMockFileSystem()
+	{
+		bool onRealFileSystemExecuted = false;
+		bool onMockFileSystemExecuted = false;
+		MockFileSystem fileSystem = new();
+		Execute.WhenRealFileSystem(fileSystem,
+			() =>
+			{
+				return onRealFileSystemExecuted = true;
+			},
+			() =>
+			{
+				return onMockFileSystemExecuted = true;
+			});
+
+		onRealFileSystemExecuted.Should().BeFalse();
+		onMockFileSystemExecuted.Should().BeTrue();
+	}
+
+	[Fact]
+	public void WhenRealFileSystem_RealFileSystem_WithActionCallback_ShouldExecuteOnRealFileSystem()
 	{
 		bool onRealFileSystemExecuted = false;
 		bool onMockFileSystemExecuted = false;
 		RealFileSystem fileSystem = new();
 		Execute.WhenRealFileSystem(fileSystem,
-			() => onRealFileSystemExecuted = true,
-			() => onMockFileSystemExecuted = true);
+			() =>
+			{
+				onRealFileSystemExecuted = true;
+			},
+			() =>
+			{
+				onMockFileSystemExecuted = true;
+			});
+
+		onRealFileSystemExecuted.Should().BeTrue();
+		onMockFileSystemExecuted.Should().BeFalse();
+	}
+
+	[Fact]
+	public void WhenRealFileSystem_RealFileSystem_WithFuncCallback_ShouldExecuteOnRealFileSystem()
+	{
+		bool onRealFileSystemExecuted = false;
+		bool onMockFileSystemExecuted = false;
+		RealFileSystem fileSystem = new();
+		Execute.WhenRealFileSystem(fileSystem,
+			() =>
+			{
+				return onRealFileSystemExecuted = true;
+			},
+			() =>
+			{
+				return onMockFileSystemExecuted = true;
+			});
 
 		onRealFileSystemExecuted.Should().BeTrue();
 		onMockFileSystemExecuted.Should().BeFalse();
