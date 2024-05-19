f6602a69084bcc9aaf76e74aed90290f6f91b049
Valentin Breuß <vbreuss@gmail.com>
2024-05-19 17:36:31 +0200

    Merge tests into one benchmark

--- Tests/Benchmarks/Testably.Abstractions.Benchmarks/Benchmarks.Constructor.cs
+++ Tests/Benchmarks/Testably.Abstractions.Benchmarks/Benchmarks.Constructor.cs
@@ -2,7 +2,6 @@
 using System.Collections.Generic;
 using System.Globalization;
 using System.IO.Abstractions;
-using System.IO.Abstractions.TestingHelpers;
 using System.Linq;
 using Testably.Abstractions.Testing;
 using Testably.Abstractions.Testing.Initializer;
@@ -10,30 +9,19 @@ using MockFileSystem = Testably.Abstractions.Testing.MockFileSystem;
 
 namespace Testably.Abstractions.Benchmarks;
 
-[RPlotExporter]
-[MemoryDiagnoser]
-public class ConstructorBenchmarks
+public partial class Benchmarks
 {
 	private readonly Dictionary<string, string> _testData = CreateTestData();
 
 	[Benchmark]
-	public IFileSystem ConstructorEmpty_TestableIO()
-		=> new System.IO.Abstractions.TestingHelpers.MockFileSystem();
-
-	[Benchmark]
 	public IFileSystem ConstructorEmpty_Testably() => new MockFileSystem();
-
-	[Benchmark]
-	public IFileSystem ConstructorWithInitialization_1000_TestableIO()
-		=> new System.IO.Abstractions.TestingHelpers.MockFileSystem(
-			_testData.ToDictionary(x => x.Key, x => new MockFileData(x.Value)));
 
 	[Benchmark]
 	public IFileSystem ConstructorWithInitialization_1000_Testably() => new MockFileSystem()
 		.Initialize()
 		.With(_testData.Select(x => new FileDescription(x.Key, x.Value)).ToArray()).FileSystem;
 
-	public static Dictionary<string, string> CreateTestData()
+	private static Dictionary<string, string> CreateTestData()
 	{
 		int filesCount = 1000;
 		int maxDirectoryDepth = 8;
