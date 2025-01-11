68a266c50f633f8e18c3ae376373bb162f9538b9
Valentin <valentin.breuss@baur.eu>
2025-01-11 14:12:53 +0100

    Add test that source generation was successful

---
+++ Tests/Testably.Abstractions.Tests/SourceGeneratorTests.cs
@@ -0,0 +1,15 @@
+﻿using System.Linq;
+
+namespace Testably.Abstractions.Tests;
+
+public sealed class SourceGeneratorTests
+{
+	[Fact]
+	public void SourceGeneratorTestsAreCreated()
+	{
+		int mockFileSystemTestsCount = typeof(SourceGeneratorTests).Assembly.GetTypes()
+			.Count(x => string.Equals(x.Name, "MockFileSystemTests", StringComparison.Ordinal));
+
+		mockFileSystemTestsCount.Should().BeGreaterThan(140);
+	}
+}
