50f3b9c6481febb1fba7da04c1388c2dda87de21
Valentin Breuß <vbreuss@gmail.com>
2025-08-11 17:45:33 +0200

    List all directories

--- Tests/Testably.Abstractions.Tests/MockFileSystemInitializationTests.cs
+++ Tests/Testably.Abstractions.Tests/MockFileSystemInitializationTests.cs
@@ -5,30 +5,41 @@ namespace Testably.Abstractions.Tests;
 
 public class MockFileSystemInitializationTests
 {
+	private readonly ITestOutputHelper _testOutputHelper;
+	public MockFileSystemInitializationTests(ITestOutputHelper testOutputHelper)
+	{
+		_testOutputHelper = testOutputHelper;
+	}
 #if FEATURE_FILESYSTEM_ENUMERATION_OPTIONS
-	[Theory]
-	[InlineData(true)]
-	[InlineData(false)]
-	public async Task EnumerateDirectories_WithEnumerationOptions_ShouldConsiderIgnoreInaccessible(
-		bool ignoreInaccessible)
+	[Fact]
+	public void EnumerateDirectories_WithEnumerationOptions_ShouldConsiderIgnoreInaccessible()
 	{
 		Skip.IfNot(Test.RunsOnWindows);
 
 		RealFileSystem fileSystem = new();
-		string path = @"C:\Windows\System32";
+		string path = @"C:\Windows";
 		EnumerationOptions enumerationOptions = new()
 		{
-			IgnoreInaccessible = ignoreInaccessible,
-			RecurseSubdirectories = true,
+			IgnoreInaccessible = true,
+			RecurseSubdirectories = false,
 		};
 
-		void Act()
+		foreach (var directory in fileSystem.Directory.GetDirectories(path, "*", enumerationOptions))
 		{
-			_ = fileSystem.Directory
-				.EnumerateDirectories(path, "*", enumerationOptions).ToList();
+			try
+			{
+				fileSystem.Directory.CreateDirectory(fileSystem.Path.Combine(directory, "_test_"));
+				_testOutputHelper.WriteLine("Access allowed for: " + directory);
+			}
+			catch (UnauthorizedAccessException)
+			{
+				_testOutputHelper.WriteLine("Access denied for: " + directory);
+			}
+			catch (Exception e)
+			{
+				_testOutputHelper.WriteLine("Error for: " + directory + " (" + e.Message + ")");
+			}
 		}
-
-		await That(Act).DoesNotThrow();
 	}
 #endif
 }
