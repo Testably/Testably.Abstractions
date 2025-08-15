58d8846cbbcc774ad27adfa9d66255a86e6c8049
Valentin Breuß <vbreuss@gmail.com>
2025-08-15 13:41:04 +0200

    feat: add support for .NET 10

---
+++ Tests/Testably.Abstractions.Parity.Tests/Net10ParityTests.cs
@@ -0,0 +1,17 @@
+﻿#if NET10_0
+using System.IO;
+
+namespace Testably.Abstractions.Parity.Tests;
+
+// ReSharper disable once UnusedMember.Global
+public class Net10ParityTests : ParityTests
+{
+	public Net10ParityTests(ITestOutputHelper testOutputHelper)
+		: base(new TestHelpers.Parity(), testOutputHelper)
+	{
+		Parity.File.MissingMethods.Add(
+			typeof(File).GetMethod(nameof(File.OpenHandle)));
+	}
+}
+
+#endif
