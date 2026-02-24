89b607d209907af84362252e8062b44beb225780
Valentin Breuß <vbreuss@gmail.com>
2026-02-24 19:59:30 +0100

    test: consider settings for skipping tests against real file system

---
+++ Tests/Testably.Abstractions.Testing.Tests/TestHelpers/FileSystemTestSettingsFixture.cs
@@ -0,0 +1,11 @@
+﻿using Testably.Abstractions.TestHelpers.Settings;
+
+namespace Testably.Abstractions.Testing.Tests.TestHelpers;
+
+[CollectionDefinition("RealFileSystemTests")]
+public class FileSystemTestSettingsFixture : ICollectionFixture<TestSettingsFixture>
+{
+	// This class has no code, and is never created. Its purpose is simply
+	// to be the place to apply [CollectionDefinition] and all the
+	// ICollectionFixture<> interfaces.
+}
